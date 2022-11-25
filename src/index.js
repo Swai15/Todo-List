a30c0b0752baf22be565ea5fee122c43e0d0ac41
Swai <swaisilvano5@gmail.com>
2022-11-25 11:12:59 +0300

    Project modal close

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-import createModal from "./modalPopup";
+import createModal,{ closeModal } from "./modalPopup";
+
 
 createModal()
+closeModal()
 
 console.log("Working");
+
+// const closeProjectModal = document.getElementById("close-projectModal")
+// const modalContainer = document.querySelector(".project-modalContainer")
+// console.log("yes");
+
+
+// function closeModal() {
+//   closeProjectModal.addEventListener("click",() => {
+//     console.log("close working");
+//     modalContainer.style.visibility = "hidden"
+//   })
+// }
+
+// closeModal()
--- src/modalPopup.js
+++ src/modalPopup.js
@@ -2,11 +2,20 @@
 
 const openProjectModal = document.querySelector(".create-project")
 const modalContainer = document.querySelector(".project-modalContainer")
+const titleValue = document.getElementById(".project-title")
+const closeProjectModal = document.getElementById("close-projectModal")
 
 
 export default function createModal() {
   openProjectModal.addEventListener("click",() => {
     modalContainer.style.visibility = "visible"
+  })
+}
+
+export function closeModal() {
+  closeProjectModal.addEventListener("click",() => {
+    console.log("close working");
+    modalContainer.style.visibility = "hidden"
   })
 }
 
