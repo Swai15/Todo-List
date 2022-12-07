e3183382638c9a33d95dd3db79399454d34c2b66
Swai <swaisilvano5@gmail.com>
2022-12-07 20:56:40 +0300

    Append functionality project item

---
+++ src/dom.js
@@ -0,0 +1,2 @@
+//everything to do with DOM manipulation
+
--- src/projectItem.js
+++ src/projectItem.js
@@ -13,23 +13,17 @@ class NewProject {
       title: this.title,
       id: Date.now(),
     };
-
     projectArrayList.push(projectArray)
     console.log(projectArrayList);
-    console.log("test complete");
   }
 }
-
-// export default function testWorking() {
-//   const book1 = new NewProject("test")
-//   book1.createProjectItem()
-// }
 
 
 const createProjectList = document.querySelector(".create-projectList");
 
 export default function createProjectFromClass() {
   createProjectList.addEventListener("click",() => {
+
     const Titleinput = document.getElementById("project-title");
     const titleText = Titleinput.value.trim();
 
@@ -38,6 +32,23 @@ export default function createProjectFromClass() {
       newProject.createProjectItem()
       Titleinput.value = "";
     }
+
+    //Append to projects
+    function appendProject() {
+      const container = document.querySelector(".projects");
+      const createProject = document.querySelector(".create-project")
+      const projectList = document.createElement("div");
+      projectList.setAttribute("class","project-list project-dynamic");
+      projectList.setAttribute("data-key","newProject.id")
+      projectList.innerHTML = `
+        <p>Project name</p>
+        <i class="fa-solid fa-trash-can"></i>
+      `
+      container.insertBefore(projectList,createProject);
+    }
+    appendProject()
+
+
   })
 }
 
